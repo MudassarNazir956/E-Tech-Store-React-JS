b58009661a222860c60bba4feaff572d775c699a
Mudassar Nazir <mudassarnazir137@gmail.com>
2024-03-10 23:19:26 +0500

    popup responsiveness fixed

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -8,7 +8,7 @@ const Popup = ({ orderPopup, handleOrderPopup }) => {
       {orderPopup && (
         <div>
           <div className="h-screen w-screen fixed top-0 left-0 bg-black/50 z-50 backdrop-blur-sm">
-            <div className="w-[500px] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 rounded-xl">
+            <div className="w-[350px] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 rounded-xl">
               {/* Header secton */}
               <div className="flex items-center justify-between tracking-widest font-sans font-semibold text-lg">
                 <h1>Place Your Order:</h1>
